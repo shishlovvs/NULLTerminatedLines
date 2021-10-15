6d474f6677d7b2cef7d37a7a79179360501cb5e1
shishlovvs <shishlovvs@mail.ru>
2021-10-15 09:45:14 +0400

    in_progress

--- NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/main.cpp
@@ -14,9 +14,16 @@ void StringLength(string* str)
 }
 
 void to_upper(char str[]);
+void to_lower(char str[]);
+void shrink(char str[]);
+void remove_symbol(char str[], const char symbol);
+bool is_palindrome(char str[]);
+#define CLASS_WORK
+//#define HOME_WORK
 
 void main()
 {
+#ifdef CLASS_WORK
 	for (int i = 0; i < 256; i++)
 	{
 		cout << i << '\t' << (char)i << endl;
@@ -27,16 +34,21 @@ void main()
 	setlocale(LC_ALL, "ru");
 
 	//char str[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
-	const int n = 20;
-	char str[20] = {};
-	//cout << "Введите строку: "; 
+	const int n = 256;
+	char str[n] = "Хорошо   живет на свете   Винни-пух";
+	cout << "Введите строку: ";
 	////cin >> str;
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	//cin.getline(str, n); //str - строка; n - размер строки
-	//cout << str << endl;
-	//cout << "Введите строку: "; 
-	
+	cin.getline(str, n); //str - строка; n - размер строки
+	cout << str << endl;
+	to_upper(str);
+	cout << str << endl;
+	shrink(str);
+	cout << str << endl;
+	cout << "Строка " << (is_palindrome(str) ? "" : "НЕ ") << " является палиндромом" << endl;
+#endif // CLASS_WORK
+
 #ifdef STRLEN
 	string str;
 	cout << "Введите что нибудь в строку: " << endl;
@@ -48,6 +60,7 @@ void main()
 
 	//или без использования функции
 
+#ifdef HOME_WORK
 	cout << "Вот есть фраза \"Hello, World!\"" << endl;
 	const char string[] = "Hello, World!";
 	cout << "Количество значений в строке: ";
@@ -58,7 +71,7 @@ void main()
 	cout << "Введите букву нижнего регистра:" << endl;
 	cin >> a;
 	a = toupper(a);
-	
+
 	cout << endl << "Та же буква в верхнем регистре: " << a;
 	cout << endl << endl;
 
@@ -72,12 +85,71 @@ void main()
 
 	to_upper(str);
 	cout << str << endl;
+#endif // HOME_WORK
+
 }
 
 void to_upper(char str[])
 {
 	for (int i = 0; str[i]; i++)
 	{
-		str[i] -= 32;
+		if (
+			str[i] > 'a' && str[i] <= 'z' ||	//элемент строки маленькая английская
+			str[i] > 'а' && str[i] <= 'я'		//или маленькая русская буква
+			)
+			str[i] -= 32;						//переводим в верхний регистр
 	}
 }
+
+void to_lower(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if(str[i] > 'a' && str[i] <= 'z' ||	//элемент строки маленькая английская
+			str[i] > 'а' && str[i] <= 'я'		//или маленькая русская буква
+			)
+			str[i] = str[i] + 32;
+	}
+}
+
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + i];
+		}
+	}
+}
+void remove_symbol(char str[], const char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+bool is_palindrome(char str[])
+{
+	
+	int n = strlen(str);
+	char* buffer = new char[n + 1]{};
+	for (int i = 0; str[i]; i++)buffer[i] = str[i];
+	to_upper(str);
+	remove_symbol(buffer, ' ');
+	n = strlen(buffer);
+	for (int i = 0; i < n / 2; i++)
+	{
+		if (buffer[i] != buffer[n - 1 - i])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[]buffer;
+	return true;
+}
+
